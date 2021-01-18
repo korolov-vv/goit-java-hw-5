b7db6b94c3e3228a3f1b14bebbac7970e7ea7c95
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2021-01-19 00:10:57 +0100

    Create LevelLoaderTest.java

---
+++ LevelLoaderTest.java
@@ -0,0 +1,48 @@
+class Level {
+    private int width;
+    private int height;
+
+    public Level(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
+
+class LevelTooBigException extends Exception{
+ LevelTooBigException() {
+ super("Level too big");
+ } 
+}
+class LevelLoader {
+    public void load(Level level) throws LevelTooBigException {
+        if(level.getWidth() * level.getHeight() <= 100000){
+          System.out.println("Level loaded");
+        }else {throw new LevelTooBigException();}
+    }
+}
+
+class LevelLoaderTest {
+    public static void main(String[] args) {
+        //Level loaded
+        try {
+            new LevelLoader().load(new Level(10, 20));
+        } catch (LevelTooBigException ex) {
+            System.out.println("Level too big");
+        }
+
+        //Level too big
+        try {
+            new LevelLoader().load(new Level(10000, 2000));
+        } catch (LevelTooBigException ex) {
+            System.out.println("Level too big");
+        }
+    }
+}
